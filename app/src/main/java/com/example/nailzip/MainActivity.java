34f52e66f1ffbfea2a81dc996fd5aa0f1ac2ca19
aksen510 <510cara@naver.com>
2022-05-29 03:33:29 +0900

    [FEAT] Add Bottom menu

--- app/src/main/java/com/example/nailzip/MainActivity.java
+++ app/src/main/java/com/example/nailzip/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.nailzip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 하단 탭 각 프래그먼트와 연결하는 코드
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
     }
 }
