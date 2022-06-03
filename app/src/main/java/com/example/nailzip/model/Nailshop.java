f7c6cb112b031fcd30faf5a5e1d3face91e03f53
aksen510 <510cara@naver.com>
2022-06-03 15:39:10 +0900

    [FEAT] Make Model of User and Nailshop

---
+++ app/src/main/java/com/example/nailzip/model/Nailshop.java
@@ -0,0 +1,50 @@
+package com.example.nailzip.model;
+
+public class Nailshop {
+
+    String email;
+    String shopname;
+    String shopphone;
+    String username;
+    String address;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getShopname() {
+        return shopname;
+    }
+
+    public void setShopname(String shopname) {
+        this.shopname = shopname;
+    }
+
+    public String getShopphone() {
+        return shopphone;
+    }
+
+    public void setShopphone(String shopphone) {
+        this.shopphone = shopphone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ app/src/main/java/com/example/nailzip/model/User.java
@@ -0,0 +1,40 @@
+package com.example.nailzip.model;
+
+public class User {
+    int position = 0;   //기본값 : 고객
+    String email;
+    String username;
+    String phonenum;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+}
