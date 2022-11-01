fb4880493b2d994da5722e3bcdde392df003b6b3
aksen510 <510cara@naver.com>
2022-11-01 16:15:57 +0900

    [FEAT] Creating UI of CreateReviewActivity

---
+++ app/src/main/java/com/example/nailzip/CreateReviewActivity.java
@@ -0,0 +1,14 @@
+package com.example.nailzip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_review);
+    }
+}
